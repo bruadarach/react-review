fca3e6c16dd929577d6229bf7f767e488987859b
Sujeong Ji <suji.sujeongji@gmail.com>
2023-06-10 00:21:14 +0900

    feat: context

--- 38-40-context/src/components/ComponentE.js
+++ 38-40-context/src/components/ComponentE.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
 import ComponentF from "./ComponentF";
+import UserContext from "./useContext";
 
 export class ComponentE extends Component {
+  // context type 4: if application supports public class fields syntax, you can use static contextType
+  static contextType = UserContext;
+
   render() {
-    return <ComponentF />;
+    // context type 3 : access the context value using this.context
+    return (
+      <div>
+        Component E context {this.context}
+        <ComponentF />
+      </div>
+    );
   }
 }
+
+// context type 2 : assign a contextType property on the class
+// ComponentE.contextType = UserContext;
 
 export default ComponentE;
--- 38-40-context/src/components/useContext.js
+++ 38-40-context/src/components/useContext.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 // 1. Create a context object
-const UserContext = React.createContext();
+const UserContext = React.createContext("Deafult Value");
 // 2. Create a provider component
 const UserProvider = UserContext.Provider;
 // 3. Create a consumer component
 const UserConsumer = UserContext.Consumer;
 
 export { UserProvider, UserConsumer };
+// context type 1 : export default UserContext;
+export default UserContext;
